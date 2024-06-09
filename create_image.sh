601b9ebd7d9272e9302e4ed6544032004bddb101
Robin Chew <me@robin.com.au>
2024-06-09 14:40:01 +0800

    more create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -2,6 +2,6 @@
 ctr=$(buildah from ubuntu:24.04);
 buildah run "$ctr" -- bash -c "echo 'Australia/Perth' > /etc/timezone"
 buildah run "$ctr" -- apt update
-buildah run "$ctr" -- apt install --assume-yes erlang-nox iproute2 make git build-essential curl erlang-dev
-buildah add "$ctr" . /var/www/mtls_server
+buildah run "$ctr" -- apt install --assume-yes erlang-nox iproute2 make git build-essential curl erlang-dev vim
 buildah commit "$ctr" "erlang-nox-mtls-ip-git"
+buildah add "$ctr" . /var/www/mtls_server
